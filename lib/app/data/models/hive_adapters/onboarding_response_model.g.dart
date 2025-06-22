0e4dbace45f5368885d5d30e80d17b403797a65f
istutiagarwal <istutiagarwal109@gmail.com>
2025-06-22 18:53:51 +0530

    water hydration reminder card

---
+++ lib/app/data/models/hive_adapters/onboarding_response_model.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../response/onboarding_response_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class OnboardingResponseModelAdapter
+    extends TypeAdapter<OnboardingResponseModel> {
+  @override
+  final int typeId = 4;
+
+  @override
+  OnboardingResponseModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return OnboardingResponseModel(
+      waterGoalLastUpdated: fields[0] as DateTime,
+      waterGoal: fields[1] as String,
+      selectedContainer: (fields[3] as List).cast<WaterLogRequestDto>(),
+      selectedContainerLastUpdated: fields[2] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, OnboardingResponseModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.waterGoalLastUpdated)
+      ..writeByte(1)
+      ..write(obj.waterGoal)
+      ..writeByte(2)
+      ..write(obj.selectedContainerLastUpdated)
+      ..writeByte(3)
+      ..write(obj.selectedContainer);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is OnboardingResponseModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
